72013a14b13d31ac0d1172d59f8df22dbbff97f1
seanhu12 <sebastian.huanca@alumnos.ucn.cl>
2022-11-17 22:55:37 -0300

    wrimero

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { TimelineCalendar,EventItem } from '@howljs/calendar-kit';
 
 export default function App() {
+  const exampleEvents= [{
+    id: '1',
+    title: 'Event 1',
+    start: '2022-11-06T09:00:05.313Z',
+    end: '2022-11-06T12:00:05.313Z',
+    color: '#A3C7D6',
+  },
+  {
+    id: '2',
+    title: 'Event 2',
+    start: '2022-11-06T11:00:05.313Z',
+    end: '2022-11-06T14:00:05.313Z',
+    color: '#B1AFFF',
+  },]
+
+    
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <SafeAreaView style={styles.container}>
+      <TimelineCalendar viewMode="day" events={exampleEvents} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: '#FFF' },
 });
